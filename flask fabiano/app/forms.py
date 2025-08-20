02fdb0f1b15232716d8289fe4faf9a441e6a7976
Amanda de Moraes <amandamoraes2611@gmail.com>
2025-08-20 07:27:59 -0300

    novo commit

---
+++ flask fabiano/app/forms.py
@@ -0,0 +1,70 @@
+from flask_bcrypt import bcrypt
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from app import db, bcrypt
+from app.models import Contato, User
+
+
+class UserForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])
+    sobrenome = StringField('Sobrenome', validators=[DataRequired()])
+    email = StringField('E-Mail', validators=[DataRequired(),Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    confirmacao_senha = PasswordField('Senha', validators=[DataRequired(), EqualTo('senha')])
+    btnSubmit = SubmitField('Cadastrar')
+
+    def validade_email(self, email):
+        if User.query.filter(email=email.data).first():
+            return ValidationError('Usuário já cadastrado com esse E-mail!!!!')
+        
+    def save(self):
+        senha_hash = bcrypt.generate_password_hash(self.senha.data).decode('utf-8')
+        senha = bcrypt.generate_password_hash(self.senha.data.encode('utf-8'))
+        user = User(
+            nome = self.nome.data,
+            sobrenome = self.sobrenome.data,
+            email = self.email.data,
+            senha = senha
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+class LoginForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(),Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    btnSubmit = SubmitField('Login')
+
+    def login(self):
+        #Recupera o usuário do email
+        user = User.query.filter_by(email=self.email.data).first()
+        #Verifica se a senha é válida
+        if user:
+            if bcrypt.check_password_hash(user.senha, self.senha.data.encode('utf-8')):
+            #retorna o usuário
+                return user
+            else:
+                raise Exception('Senha Incorreta!!!')
+        else:
+            raise Exception('Usuário não encontrado!!!')
+
+
+class ContatoForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])
+    email = StringField('E-Mail', validators=[DataRequired(),Email()])
+    assunto = StringField('Assunto', validators=[DataRequired()])
+    mensagem = StringField('Mensagem', validators=[DataRequired()])
+    btnSubmit = SubmitField('Enviar')
+    def save(self):
+        contato = Contato(
+            nome = self.nome.data,
+            email = self.email.data,
+            assunto = self.assunto.data,
+            mensagem = self.mensagem.data
+        )
+
+        
+        db.session.add(contato)
+        db.session.commit()
